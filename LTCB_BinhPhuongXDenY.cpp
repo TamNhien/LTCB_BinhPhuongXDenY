95d7c0c084219f850539fe040a219f6ca1c05a4e
TamNhien <canhthien91@gmail.com>
2024-03-07 20:11:08 +0700

    Add project files.

---
+++ LTCB_BinhPhuongXDenY.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cout << "Nhap so nguyen x: "; cin >> x;
+    cout << "Nhap so nguyen y: "; cin >> y;
+
+    int sum = 0;
+    for (int i = x; i <= y; ++i) {
+        sum += i * i;
+    }
+    cout << "Tong binh phuong cac so tu " << x << " den " << y << " la: " << sum << endl;
+    return 0;
+}
